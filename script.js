0c398065d6b55a98d7c3e244864994563d837a47
Your GitHub Username <mdarasimi52@gmail.com>
2025-11-20 12:29:06 +0100

    Updated code and fixed issues

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ $(document).ready(function () {
 
   // typing animation script
   var typed = new Typed(".typing", {
-    strings: ["Web Developer", "Instructor", "Blogger", "Video Editor", "Developer", "Keyboardist"],
+    strings: ["Web Developer", "Instructor", "Blogger", "Video Editor", "Developer",],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
@@ -37,7 +37,7 @@ $(document).ready(function () {
 
 
    var typed = new Typed(".typing-2", {
-    strings: ["Web Developer", "Instructor", "Blogger", "Video Editor", "Developer", "Keyboardist"],
+    strings: ["Web Developer", "Instructor", "Blogger", "Video Editor", "Developer",],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
